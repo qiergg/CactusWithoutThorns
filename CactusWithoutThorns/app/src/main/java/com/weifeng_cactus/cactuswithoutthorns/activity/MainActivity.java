9e4556c4ad8a8c35949c87b0ebcb2babeac45c6e
MaiYa <maiya@MaiYadeMacBook-Pro.local>
2016-07-27 19:24:14 +0800

    轮播图

--- CactusWithoutThorns/app/src/main/java/com/weifeng_cactus/cactuswithoutthorns/activity/MainActivity.java
+++ CactusWithoutThorns/app/src/main/java/com/weifeng_cactus/cactuswithoutthorns/activity/MainActivity.java
@@ -1,11 +1,18 @@
 package com.weifeng_cactus.cactuswithoutthorns.activity;
 
 import android.os.Bundle;
+import android.util.TypedValue;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 
 import com.aebiz.sdk.Base.BaseFragmentActivity;
 import com.aebiz.sdk.UIKit.component.refresh.PullToRefreshBase;
 import com.aebiz.sdk.UIKit.component.refresh.PullToRefreshListView;
+import com.weifeng_cactus.cactuswithoutthorns.Fragment.ImageBanner;
+import com.weifeng_cactus.cactuswithoutthorns.Model.IndexCarouselItem;
 import com.weifeng_cactus.cactuswithoutthorns.R;
 import com.weifeng_cactus.cactuswithoutthorns.adapter.MessageAdapter;
 
@@ -38,6 +45,27 @@ messageListView.setOnRefreshListener(new PullToRefreshBase.OnRefreshListener2<Li
         messageAdapter.notifyDataSetChanged();
     }
 });
+    }
+
+    private void generateComponentView(IndexCarouselItem[] indexCarouselItems, ViewGroup viewGroup) {
+        // 广告banner
+        if (indexCarouselItems.length > 0) {
+            ImageBanner imageBanner = new ImageBanner(getApplicationContext());
+            imageBanner.setData(indexCarouselItems);
+            viewGroup.addView(imageBanner.getView(), ViewGroup.LayoutParams.MATCH_PARENT, (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 167, getApplicationContext().getResources().getDisplayMetrics()));
+            View view = new View(getApplicationContext());
+            view.setBackgroundColor(getResources().getColor(R.color.default_separate_gray));
+            viewGroup.addView(view, ViewGroup.LayoutParams.MATCH_PARENT, (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_PX, 1, getApplicationContext().getResources().getDisplayMetrics()));
+        }
+
+        View menuView = LayoutInflater.from(getApplicationContext()).inflate(R.layout.view_home_header, null);
+        viewGroup.addView(menuView, ViewGroup.LayoutParams.MATCH_PARENT, (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_PX, ViewGroup.LayoutParams.WRAP_CONTENT, getApplicationContext().getResources().getDisplayMetrics()));
+    }
+
+    private View perpareHeadView(IndexCarouselItem[] indexCarouselItems) {
+        LinearLayout headView = (LinearLayout) this.getLayoutInflater().inflate(R.layout.headview_home, null);
+        generateComponentView(indexCarouselItems, headView);
+        return headView;
     }
 
 
